60b7692ee68cee5eed3478545507ec4f1e242eee
Usuario <Usuario@DESKTOP-C6647AB>
2023-03-06 12:19:51 +0100

    Mi primer commit CadenasDeLlanoSerranoJorge2223

--- src/vehiculo/Main.java
+++ src/vehiculo/Main.java
@@ -2,23 +2,26 @@
 package vehiculo;
 
 /**
- *
+ * Clase main del programa. Crea un tipo de vehículo y luego opera sobre él.
  * @author IES Aguadulce
  */
 public class Main {
-    
+    /**
+     * Método main.
+     * 
+     * Este es el método principal del programa. 
+     * 
+     */
       public static void main(String[] args) {
         VehiculoCadenasDeLlanoSerranoJorge2223 miVehiculoCadenasDeLlanoSerranoJorge2223;
         int stockActual;
         
         miVehiculoCadenasDeLlanoSerranoJorge2223 = new VehiculoCadenasDeLlanoSerranoJorge2223("Seat",18000,100);
-        operativaVehiculosCadenasDeLlanoSerranoJorge2223(miVehiculoCadenasDeLlanoSerranoJorge2223); 
+        operativaVehiculosCadenasDeLlanoSerranoJorge2223(miVehiculoCadenasDeLlanoSerranoJorge2223, 50); 
     }
 
-    private static void operativaVehiculosCadenasDeLlanoSerranoJorge2223(VehiculoCadenasDeLlanoSerranoJorge2223 miVehiculoCadenasDeLlanoSerranoJorge2223) {
+    private static void operativaVehiculosCadenasDeLlanoSerranoJorge2223(VehiculoCadenasDeLlanoSerranoJorge2223 miVehiculoCadenasDeLlanoSerranoJorge2223, int cantidad) {
         int stockActual;
-        int cantidad;
-        cantidad=50;
         try
         {
             System.out.println("Venta de Vehiculos");
